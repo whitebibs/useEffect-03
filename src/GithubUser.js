4a3141f7921295a4a86b867ac7f9fcd2ef16fc9a
whitebibs <115084982+whitebibs@users.noreply.github.com>
2023-02-14 16:16:39 +0100

    Update GithubUser.js

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,28 +2,19 @@ import React, { useState, useEffect } from 'react';
 
 export function GithubUser ( {username} ) {
   const [user, setUser] = useState({});
-  const [error, setError] = useState(false);
-
-  async function fetchData(username) {
-    try{
-      const response = await fetch (`https://api.github.com/users/${username}`);
-      const data= await response.json();
-      setUser(data);
-      }catch(error){
-     setError(error)
-      }
-    }
 
   useEffect(() => {
-    fetchData(username);
-  }, [username])
+   async function fetchUser(){
+    const response = await fetch (`https://api.github.com/users/${username}`)
+    const data= await response.json();
+    setUser(data)
+  }
+  fetchUser()
+  },[username] )
   return (
     <div>
       <p>Username: {user.login}</p>
       <p>Name: {user.name}</p>
-      {error && <h5>{error.message}</h5>}
     </div>
   )
   }
-
-
